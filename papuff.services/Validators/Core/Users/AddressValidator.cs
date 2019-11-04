5b1dd942df76a55777f28e7796bfb2bbbc508b51
datasuricata <lucas.moraes.dev@gmail.com>
2019-11-03 23:02:28 -0300

    single register

--- papuff.services/Validators/Core/Users/AddressValidator.cs
+++ papuff.services/Validators/Core/Users/AddressValidator.cs
@@ -1,23 +1,23 @@
-﻿using FluentValidation;
+using FluentValidation;
 using papuff.domain.Core.Users;
 
 namespace papuff.services.Validators.Core.Users {
     public class AddressValidator : AbstractValidator<Address> {
-        public AddressValidator() {
-            RuleFor(r => r.AddressLine).NotNull().NotEmpty()
-                .WithMessage("Linha de endereço deve ser informado.");
+        public AddressValidator () {
+            RuleFor (r => r.AddressLine).NotNull ().NotEmpty ()
+                .WithMessage ("Linha de endereço deve ser informado.");
 
-            RuleFor(r => r.City).NotNull().NotEmpty()
-                .WithMessage("Informe uma cidade.");
+            RuleFor (r => r.City).NotNull ().NotEmpty ()
+                .WithMessage ("Informe uma cidade.");
 
-            RuleFor(r => r.Country).NotNull().NotEmpty()
-                .WithMessage("Informe o país.");
+            RuleFor (r => r.Country).NotNull ().NotEmpty ()
+                .WithMessage ("Informe o país.");
 
-            RuleFor(r => r.PostalCode).NotNull().NotEmpty()
-                .WithMessage("Informe o código postal.");
+            RuleFor (r => r.PostalCode).NotNull ().NotEmpty ()
+                .WithMessage ("Informe o código postal.");
 
-            RuleFor(r => r.StateProvince).NotNull().NotEmpty()
-                .WithMessage("Estado é obrigatório");
+            RuleFor (r => r.StateProvince).NotNull ().NotEmpty ()
+                .WithMessage ("Estado é obrigatório");
         }
     }
 }
--- papuff.services/Validators/Core/Users/PaymentValidator.cs
+++ papuff.services/Validators/Core/Users/PaymentValidator.cs
@@ -1,20 +1,20 @@
-﻿using FluentValidation;
+using FluentValidation;
 using papuff.domain.Core.Wallets;
 
 namespace papuff.services.Validators.Core.Users {
     public class PaymentValidator : AbstractValidator<Payment> {
-        public PaymentValidator() {
-            RuleFor(r => r.WalletId)
-                .NotNull().NotEmpty().WithMessage("Carteira de destino é obrigatória, contate o suporte.");
+        public PaymentValidator () {
+            RuleFor (r => r.WalletId)
+                .NotNull ().NotEmpty ().WithMessage ("Carteira de destino é obrigatória, contate o suporte.");
 
-            RuleFor(r => r.Card)
-                .NotNull().NotEmpty().WithMessage("Informe o número do cartão.");
+            RuleFor (r => r.Card)
+                .NotNull ().NotEmpty ().WithMessage ("Informe o número do cartão.");
 
-            RuleFor(r => r.Code)
-                .NotNull().NotEmpty().WithMessage("Informe o código de segurança.");
+            RuleFor (r => r.Code)
+                .NotNull ().NotEmpty ().WithMessage ("Informe o código de segurança.");
 
-            RuleFor(r => r.Document)
-                .NotNull().NotEmpty().WithMessage("Cpf é obrigatório.");
+            RuleFor (r => r.Document)
+                .NotNull ().NotEmpty ().WithMessage ("Cpf é obrigatório.");
         }
     }
 }
